46f6b0c8630fd782b34f6436708a14f2ce762e7d
Kirill <keksonoid98@gmail.com>
2020-12-01 13:50:16 +0600

    autentification

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 // 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
@@ -12,21 +14,30 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+	uri: 'http://localhost:4000'
 })
 
-// 3
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN)
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	}
+})
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
 })
 
 // 4
 ReactDOM.render(
-  <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </BrowserRouter>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</BrowserRouter>,
+	document.getElementById('root')
 )
