eaf9ffbd0dcab121e9dba7b82478f8746311d260
Kirill <keksonoid98@gmail.com>
2020-12-01 14:17:02 +0600

    updating votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,16 +3,26 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
-	feed {
-	  links {
-		id
-		createdAt
-		url
-		description
-	  }
-	}
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
   }
 `
 class LinkList extends Component {
@@ -25,15 +35,24 @@ class LinkList extends Component {
 	
 		  const linksToRender = data.feed.links
 	
-		  return (
-			<div>
-			  {linksToRender.map(link => <Link key={link.id} link={link} />)}
-			</div>
-		  )
+			return (
+				<div>
+					{linksToRender.map((link, index) => (
+					<Link key={link.id} link={link} index={index} updateStoreAfterVote={this._updateCacheAfterVote}/>
+					))}
+				</div>
+			)
 		}}
 	  </Query>
 	)
   }
-}
+	_updateCacheAfterVote = (store, createVote, linkId) => {
+		const data = store.readQuery({ query: FEED_QUERY })
 
+		const votedLink = data.feed.links.find(link => link.id === linkId)
+		votedLink.votes = createVote.link.votes
+
+		store.writeQuery({ query: FEED_QUERY, data })
+	}
+}
 export default LinkList
